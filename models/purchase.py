55e09fe38f8fc6c4e29a8d8931e0b0733b5f3bc1
dani-ddl <dani--ef-@hotmail.com>
2019-12-23 10:45:32 +0100

    arreglo precio

--- models/purchase.py
+++ models/purchase.py
@@ -23,8 +23,21 @@ class PurchaseOrderLine(models.Model):
     @api.onchange('product_id')
     def onchange_product_id(self):
         result = super(PurchaseOrderLine, self).onchange_product_id()
-        self.price_unit = self.product_id.standard_price
         logging.info('***** Precio venta *****')
+        logging.info(result)
         logging.info( self.price_unit)
+
         self.price_unit = self.product_id.standard_price
+
+        return result
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        result = super(PurchaseOrderLine, self)._onchange_quantity()
+        logging.info('***** Precio venta df2 *****')
+        logging.info(result)
+        logging.info(self.price_unit)
+
+        self.price_unit = self.product_id.standard_price
+
         return result
